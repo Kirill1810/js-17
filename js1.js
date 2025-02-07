42812d580d4a31eca156af53ccdbb4a1741c763b
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2025-02-07 21:02:12 +0200

    zzzz

--- js1.js
+++ js1.js
@@ -1,53 +1,34 @@
 //1
 
-const but = document.getElementsByClassNam('myButton');
-const textInput = document.getElementsByClassNam('textInput');
- but.addEventListener('click', function() {
-   
-     but.textContent = textInput.value;
- });
+const takeBtm = document.querySelector("#text")
+const take = document.querySelector("#btn")
+take.textContent = takeBtm.textContent
+console.log(takeBtm)
 
 
 //2
 
+const takeImg = document.querySelector("#img")
 
- const image = document. getElementsByClassName('img');
-        const butto = document. getElementsByClassName('imgg');
-
-      
-        butto.addEventListener('click', function() {
-      
-            image.src = 'https://http.cat/[status_code]';
-
-        });
-
-
-
+takeImg.src = "https://foni.papik.pro/uploads/posts/2024-10/foni-papik-pro-yvru-p-kartinki-udivlennii-kot-na-prozrachnom-fon-6.png"
 
 //3
 
 
-        const link = document.getElementsByClassName('myLink');
-        const img = document.getElementsByClassName('myImg');
-        const button = document.getElementsByClassName('swap');
 
-      
-        button.addEventListener('click', function() {
-            link.href = 'https://www.youtube.com/watch?v=MdNhTUo9MlY';
-            image.setAttribute('alt',);
-        });
+const takeHref = document.querySelector("#myLink")
+const takeAlt = document.querySelector("#myImg")
 
-        
-        //4
+takeAlt.alt = "нема кота все"
+takeHref.href = "https://kor.ill.in.ua/m/610x385/2781367.jpg"
 
 
+//4
 
-        
-        const first = document.querySelector('all'); 
-         const click = document.getElementsByClassName('cange');
- 
-     
-         click.addEventListener('click', function() {
-      
-             firstListItem.textContent = 'Новий текст для першого елемента';
-         });
+
+const takeUl = document.querySelector("#all")
+const firstElement = takeUl.firstElementChild
+firstElement.textContent = "ананас "
+
+console.log(takeUl)
+
